f3b3cfdccc1a552d6269de59cc2a02ab71ea5d09
rak <rak29007@gmail.com>
2022-11-10 02:43:21 +0000

    Some fixes and minor changes to functions. Reduced number of function parameters

--- code.py
+++ code.py
@@ -14,7 +14,7 @@ world = pd.read_csv("E:/Herts/ADS1/Assignment 1/ADS1_Assignment_Visualisation/ca
 
 #Creating seperate dataframes for each country from input data
 uk = world.iloc[5790:5820]
-uk["Total"] = uk.sum(axis=1)-uk["Year"]
+uk["Total"] = uk.sum(axis=1) - uk["Year"]
 germany = world.iloc[2010:2040]
 germany["Total"] = germany.sum(axis=1) - germany["Year"]
 france = world.iloc[1890:1920]
@@ -50,23 +50,43 @@ def line_plot_different_diseases_uk(y_axis, reason_1, reason_2, reason_3):
     plt.plot(uk[y_axis],uk[reason_2])
     plt.plot(uk[y_axis],uk[reason_3])
     plt.xlabel("Year")
+    plt.xlim(1990,2019)
     plt.ylabel("Deaths")
     plt.legend(["Alcohol abuse", "Violence", "Drowning"])
     plt.savefig("line2.png")
     plt.show()
 
-def box_plot(y_axis, x_axis):
+def box_plot(data):
     """
-        This function creates box plots for cardiovascular disease deaths with 
-        x and y axis given through arguments. Graphs are also saved as images 
-        to local directory
+        This function creates box plots for cardiovascular disease deaths for certain countries.
+        List of dataframes are given as argument.Graphs are also saved as images to local directory
     """
-    data = [uk[x_axis], germany[x_axis],france[x_axis],italy[x_axis]]
     plt.figure()
     plt.boxplot([data[0] , data[1] , data[2], data[3]], labels=["UK", "DE", "FR", "IT"])
     plt.ylabel("Deaths")
     plt.savefig("box.png")
     plt.show()
+
+def bar_chart(cause):
+    """
+        his function creates bar charts for a certain cuntries where cause of death 
+        is given as argument. Graph is also saved as images to local directory
+    """
+    plt.figure()
+    plt.hist(uk[cause], label = "UK", density=True, alpha=0.8)
+    plt.hist(germany[cause], label = "DE", density=True, alpha=0.8)
+    plt.hist(france[cause], label = "FR", density=True, alpha=0.8)
+    plt.hist(italy[cause], label = "IT", density=True, alpha=0.8)
+    plt.legend()
+    plt.xlabel("CVD per Total Deaths")
+    plt.xlabel("No. Of Deaths")
+    plt.savefig("bar.png")
+    plt.show()
+
+
+#creating a new array to pass to box plot function
+cause = "Cardiovascular Diseases"
+data = [uk[cause], germany[cause],france[cause],italy[cause]]
 
 #Creating new row Cardiovascular Diseases deaths per total deaths for each country
 uk["cvd per total deaths"] = uk["Cardiovascular Diseases"]/uk["Total"]
@@ -74,25 +94,8 @@ germany["cvd per total deaths"] = germany["Cardiovascular Diseases"]/germany["To
 france["cvd per total deaths"] = france["Cardiovascular Diseases"]/france["Total"]
 italy["cvd per total deaths"] = italy["Cardiovascular Diseases"]/italy["Total"]
 
-def bar_chart(y_axis, x_axis):
-    """
-        his function creates bar charts for cardiovascular disease deaths 
-        with x and y axis given through arguments. Graphs are also saved as 
-        images to local directory
-    """
-    plt.figure()
-    plt.hist(uk["cvd per total deaths"], label = "UK", density=True, alpha=0.8)
-    plt.hist(germany["cvd per total deaths"], label = "DE", density=True, alpha=0.8)
-    plt.hist(france["cvd per total deaths"], label = "FR", density=True, alpha=0.8)
-    plt.hist(italy["cvd per total deaths"], label = "IT", density=True, alpha=0.8)
-    plt.legend()
-    plt.xlabel("CVD per Total Deaths")
-    plt.xlabel("No. Of Deaths")
-    plt.savefig("bar.png")
-    plt.show()
-
 #Calling functions to create plots
 lineplot_different_countries("Year", "Cardiovascular Diseases")
 line_plot_different_diseases_uk("Year", "Alcohol Use Disorders", "Interpersonal Violence", "Drowning")
-box_plot("Year", "Cardiovascular Diseases")
-bar_chart("Year", "Cardiovascular Diseases")
+box_plot(data)
+bar_chart("cvd per total deaths")
